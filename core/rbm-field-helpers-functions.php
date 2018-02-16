d13f96b7ebaceb82c36021e61b9d4630abe8bb3c
Eric Defore <eric.defore@gmail.com>
2018-02-16 13:29:40 -0500

    Working wrapper

---
+++ core/rbm-field-helpers-functions.php
@@ -0,0 +1,271 @@
+<?php
+/**
+ * Provides helper functions for RBM FH
+ * These are mostly the same as Legacy for simplicity, but the parameters are not Legacy. Watch out!
+ *
+ * @since	  1.0.0
+ *
+ * @package	RBM_Field_Helpers_Wrapper
+ * @subpackage RBM_Field_Helpers_Wrapper/core
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
+/**
+ * Quick access to plugin field helpers.
+ *
+ * @since {{VERSION}}
+ *
+ * @return RBM_FieldHelpers
+ */
+function rbm_fh() {
+	return RBMFIELDHELPERSWRAPPER()->field_helpers;
+}
+
+/**
+ * Initializes a field group for automatic saving.
+ *
+ * @since {{VERSION}}
+ *
+ * @param $group
+ */
+function rbm_fh_init_field_group( $group ) {
+	rbm_fh()->fields->save->initialize_fields( $group );
+}
+
+/**
+ * Gets a meta field helpers field.
+ *
+ * @since {{VERSION}}
+ *
+ * @param string $name Field name.
+ * @param string|int $post_ID Optional post ID.
+ * @param mixed $default Default value if none is retrieved.
+ * @param array $args
+ *
+ * @return mixed Field value
+ */
+function rbm_fh_get_field( $name, $post_ID = false, $default = '', $args = array() ) {
+    $value = rbm_fh()->fields->get_meta_field( $name, $post_ID, $args );
+    return $value !== false ? $value : $default;
+}
+
+/**
+ * Gets a option field helpers field.
+ *
+ * @since {{VERSION}}
+ *
+ * @param string $name Field name.
+ * @param mixed $default Default value if none is retrieved.
+ * @param array $args
+ *
+ * @return mixed Field value
+ */
+function rbm_fh_get_option_field( $name, $default = '', $args = array() ) {
+	$value = rbm_fh()->fields->get_option_field( $name, $args );
+	return $value !== false ? $value : $default;
+}
+
+/**
+ * Outputs a text field.
+ *
+ * @since {{VERSION}}
+ *
+ * @param array $args
+ */
+function rbm_fh_do_field_text( $args = array() ) {
+	rbm_fh()->fields->do_field_text( $args['name'], $args );
+}
+
+/**
+ * Outputs a textarea field.
+ *
+ * @since {{VERSION}}
+ *
+ * @param array $args
+ */
+function rbm_fh_do_field_textarea( $args = array() ) {
+	rbm_fh()->fields->do_field_textarea( $args['name'], $args );
+}
+
+/**
+ * Outputs a checkbox field.
+ *
+ * @since {{VERSION}}
+ *
+ * @param array $args
+ */
+function rbm_fh_do_field_checkbox( $args = array() ) {
+	rbm_fh()->fields->do_field_checkbox( $args['name'], $args );
+}
+
+/**
+ * Outputs a toggle field.
+ *
+ * @since {{VERSION}}
+ *
+ * @param array $args
+ */
+function rbm_fh_do_field_toggle( $args = array() ) {
+	rbm_fh()->fields->do_field_toggle( $args['name'], $args );
+}
+
+/**
+ * Outputs a radio field.
+ *
+ * @since {{VERSION}}
+ *
+ * @param array $args
+ */
+function rbm_fh_do_field_radio( $args = array() ) {
+	rbm_fh()->fields->do_field_radio( $args['name'], $args );
+}
+
+/**
+ * Outputs a select field.
+ *
+ * @since {{VERSION}}
+ *
+ * @param array $args
+ */
+function rbm_fh_do_field_select( $args = array() ) {
+	rbm_fh()->fields->do_field_select( $args['name'], $args );
+}
+
+/**
+ * Outputs a number field.
+ *
+ * @since {{VERSION}}
+ *
+ * @param array $args
+ */
+function rbm_fh_do_field_number( $args = array() ) {
+	rbm_fh()->fields->do_field_number( $args['name'], $args );
+}
+
+/**
+ * Outputs an image field.
+ *
+ * @since {{VERSION}}
+ *
+ * @param array $args
+ */
+function rbm_fh_do_field_media( $args = array() ) {
+	rbm_fh()->fields->do_field_media( $args['name'], $args );
+}
+
+/**
+ * Outputs a datepicker field.
+ *
+ * @since {{VERSION}}
+ *
+ * @param array $args
+ */
+function rbm_fh_do_field_datepicker( $args = array() ) {
+	rbm_fh()->fields->do_field_datepicker( $args['name'], $args );
+}
+
+/**
+ * Outputs a timepicker field.
+ *
+ * @since {{VERSION}}
+ *
+ * @param array $args
+ */
+function rbm_fh_do_field_timepicker( $args = array() ) {
+	rbm_fh()->fields->do_field_timepicker( $args['name'], $args );
+}
+
+/**
+ * Outputs a datetimepicker field.
+ *
+ * @since {{VERSION}}
+ *
+ * @param array $args
+ */
+function rbm_fh_do_field_datetimepicker( $args = array() ) {
+	rbm_fh()->fields->do_field_datetimepicker( $args['name'], $args );
+}
+
+/**
+ * Outputs a colorpicker field.
+ *
+ * @since {{VERSION}}
+ *
+ * @param array $args
+ */
+function rbm_fh_do_field_colorpicker( $args = array() ) {
+	rbm_fh()->fields->do_field_colorpicker( $args['name'], $args );
+}
+
+/**
+ * Outputs a list field.
+ *
+ * @since {{VERSION}}
+ *
+ * @param array $args
+ */
+function rbm_fh_do_field_list( $args = array() ) {
+	rbm_fh()->fields->do_field_list( $args['name'], $args );
+}
+
+/**
+ * Outputs a hidden field.
+ *
+ * @since {{VERSION}}
+ *
+ * @param array $args
+ */
+function rbm_fh_do_field_hidden( $args = array() ) {
+	rbm_fh()->fields->do_field_hidden( $args['name'], $args );
+}
+
+/**
+ * Outputs a table field.
+ *
+ * @since {{VERSION}}
+ *
+ * @param array $args
+ */
+function rbm_fh_do_field_table( $args = array() ) {
+	rbm_fh()->fields->do_field_table( $args['name'], $args );
+}
+
+/**
+ * Outputs a HTML field.
+ *
+ * @since {{VERSION}}
+ *
+ * @param array $args
+ */
+function rbm_fh_do_field_html( $args = array() ) {
+	rbm_fh()->fields->do_field_html( $args['name'], $args );
+}
+
+/**
+ * Outputs a repeater field.
+ *
+ * @since {{VERSION}}
+ *
+ * @param mixed $values
+ */
+function rbm_fh_do_field_repeater( $args = array() ) {
+	rbm_fh()->fields->do_field_repeater( $args['name'], $args );
+}
+
+/**
+ * Outputs a String if a Callback Function does not exist for an Options Page Field
+ *
+ * @since {{VERSION}}
+ *
+ * @param array $args
+ */
+function rbm_fh_missing_callback( $args ) {
+	
+	printf( 
+		_x( 'A callback function called "rbm_fh_do_field_%s" does not exist.', '%s is the Field Type', 'rbm-field-helpers-wrapper' ),
+		$args['type']
+	);
+		
+}
--- rbm-field-helpers-wrapper.php
+++ rbm-field-helpers-wrapper.php
@@ -23,16 +23,22 @@ if ( ! class_exists( 'RBM_Field_Helpers_Wrapper' ) ) {
 	class RBM_Field_Helpers_Wrapper {
 		
 		/**
-		 * @var			RBM_Field_Helpers_Wrapper $plugin_data Holds Plugin Header Info
+		 * @var			array $plugin_data Holds Plugin Header Info
 		 * @since		1.0.0
 		 */
 		public $plugin_data;
 		
 		/**
-		 * @var			RBM_Field_Helpers_Wrapper $admin_errors Stores all our Admin Errors to fire at once
+		 * @var			array $admin_errors Stores all our Admin Errors to fire at once
 		 * @since		1.0.0
 		 */
 		private $admin_errors;
+		
+		/**
+		 * @var			RBM_FieldHelpers RBM FH
+		 * @since		{{VERSION}}
+		 */
+		public $field_helpers;
 
 		/**
 		 * Get active instance
@@ -161,6 +167,47 @@ if ( ! class_exists( 'RBM_Field_Helpers_Wrapper' ) ) {
 		 * @return	  void
 		 */
 		private function require_necessities() {
+			
+			require_once __DIR__ . '/core/rbm-field-helpers/rbm-field-helpers.php';
+		
+			$this->field_helpers = new RBM_FieldHelpers( array(
+				'ID'   => '_rbm', // Your Theme/Plugin uses this to differentiate its instance of RBM FH from others when saving/grabbing data
+				'l10n' => array(
+					'field_table'    => array(
+						'delete_row'    => __( 'Delete Row', 'rbm-field-helpers-wrapper' ),
+						'delete_column' => __( 'Delete Column', 'rbm-field-helpers-wrapper' ),
+					),
+					'field_select'   => array(
+						'no_options'       => __( 'No select options.', 'rbm-field-helpers-wrapper' ),
+						'error_loading'    => __( 'The results could not be loaded', 'rbm-field-helpers-wrapper' ),
+						/* translators: %d is number of characters over input limit */
+						'input_too_long'   => __( 'Please delete %d character(s)', 'rbm-field-helpers-wrapper' ),
+						/* translators: %d is number of characters under input limit */
+						'input_too_short'  => __( 'Please enter %d or more characters', 'rbm-field-helpers-wrapper' ),
+						'loading_more'     => __( 'Loading more results...', 'rbm-field-helpers-wrapper' ),
+						/* translators: %d is maximum number items selectable */
+						'maximum_selected' => __( 'You can only select %d item(s)', 'rbm-field-helpers-wrapper' ),
+						'no_results'       => __( 'No results found', 'rbm-field-helpers-wrapper' ),
+						'searching'        => __( 'Searching...', 'rbm-field-helpers-wrapper' ),
+					),
+					'field_repeater' => array(
+						'collapsable_title' => __( 'New Row', 'rbm-field-helpers-wrapper' ),
+						'confirm_delete'    => __( 'Are you sure you want to delete this element?', 'rbm-field-helpers-wrapper' ),
+						'delete_item'       => __( 'Delete', 'rbm-field-helpers-wrapper' ),
+						'add_item'          => __( 'Add', 'rbm-field-helpers-wrapper' ),
+					),
+					'field_media'    => array(
+						'button_text'        => __( 'Upload / Choose Media', 'rbm-field-helpers-wrapper' ),
+						'button_remove_text' => __( 'Remove Media', 'rbm-field-helpers-wrapper' ),
+						'window_title'       => __( 'Choose Media', 'rbm-field-helpers-wrapper' ),
+					),
+					'field_checkbox' => array(
+						'no_options_text' => __( 'No options available.', 'rbm-field-helpers-wrapper' ),
+					),
+				),
+			) );
+			
+			require_once __DIR__ . '/core/rbm-field-helpers-functions.php';
 			
 		}
 		
